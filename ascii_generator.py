cc6096f08e4a65529deb9d982090b6669a82689e
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-12-18 16:28:12 -0600

    Started project
    Project started

---
+++ ascii_generator.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np
+from PIL import Image  
+
+def assign_character(value):
+    densityMap = "Ñ@#W$9876543210?!abc;:+=-,._ "
+    #densityMap = "█▓▒░▪▫ "
+    return densityMap[math.floor(np.interp(value, [0, 256], [0, len(densityMap)]))]
+
+if __name__ == "__main__":
+    for i in range(256):
+        print(i, " = ", assign_character(i))
