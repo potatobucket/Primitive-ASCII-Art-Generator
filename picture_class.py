d0b6f7f2c1a04b7e9fa4588420be39f28c143df9
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-12-18 18:49:59 -0600

    Made image scalable and in the correct orientation
    It's upright now!

--- picture_class.py
+++ picture_class.py
@@ -4,7 +4,7 @@ def get_pixel_data(picture, mode = "L"):
     with Image.open(picture) as img:
         picWidth, picHeight = img.size[0], img.size[1]
         img = img.convert(mode)
-        pixelData = [img.getpixel((column, row)) for row in range(picWidth) for column in range(picHeight)]
+        pixelData = [img.getpixel((column, row)) for row in range(picHeight) for column in range(picWidth)]
     return pixelData, picWidth, picHeight
 
 class Picture:
