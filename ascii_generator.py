8226c5086d0280e06ed6777c597841b44a39337e
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-12-19 00:21:53 -0600

    String maker and density maps
    Added a function to create the final string and also added some extra density maps.

--- ascii_generator.py
+++ ascii_generator.py
@@ -2,9 +2,23 @@ import math
 import numpy as np
 
 def assign_character(value):
-    densityMap = "Ñ@#W$9876543210?!abc;:+=-,._ "
+    #densityMap = "Ñ@#W$9876543210?!abc;:+=-,._ "
     #densityMap = "█▓▒░▪▫ "
+    #densityMap = "@QB#NgWM8RDHdOKq9$6khEPXwmeZaoS2yjufF]}{tx1zv7lciL/\\|?*>r^;:_\"~,'.-` "
+    densityMap = "█▉▇▓▊▆▅▌▚▞▀▒▐▍▃▖▂░▁▏ "
+    #densityMap = "◙◘■▩●▦▣◚◛◕▨▧◉▤◐◒▮◍◑▼▪◤▬◗◭◖◈◎◮◊◫▰◄◯□▯▷▫▽◹△◁▸▭◅▵◌▱▹▿◠◃◦◟◞◜ "
+    #densityMap = "╬╠╫╋║╉╩┣╦╂╳╇╈┠╚┃╃┻╅┳┡┢┹╀╧┱╙┗┞┇┸┋┯┰┖╲╱┎╘━┭┕┍┅╾│┬┉╰╭╸└┆╺┊─╌┄┈╴╶  "
+    #densityMap = "ぽぼゑぜぬあおゆぎゐはせぢがきぱびほげばゟぁたかぞぷれひずどらさでけぉちごえすゎにづぇとょついこぐうぅぃくっしへゞゝ゚゙ "
     return densityMap[math.floor(np.interp(value, [0, 256], [0, len(densityMap)]))]
+
+def make_string(data, width):
+    newString = ""
+    for index,datum in enumerate(data):
+        if (index + 1) % width == 0:
+            newString += f"{datum}\n"
+        else:
+            newString += f"{datum}"
+    return newString
 
 if __name__ == "__main__":
     for i in range(256):
